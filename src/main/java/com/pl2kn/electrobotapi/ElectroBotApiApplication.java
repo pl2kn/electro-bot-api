cd121b66caff4e10bf5f355330a573195116dc99
pl2kn <pavel.konkin.dev@gmail.com>
2022-10-29 16:50:04 +0300

    init

---
+++ src/main/java/com/pl2kn/electrobotapi/ElectroBotApiApplication.java
@@ -0,0 +1,13 @@
+package com.pl2kn.electrobotapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectroBotApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ElectroBotApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/pl2kn/electrobotapi/ElectroBotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl2kn.electrobotapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectroBotApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
